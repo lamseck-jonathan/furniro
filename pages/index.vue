5946149532c8fd4144e4099c3e1c567463294b28
jonathan-lamseck <96185904+lamseck-jonathan@users.noreply.github.com>
2025-08-31 01:17:25 +0300

    feature-section

--- pages/index.vue
+++ pages/index.vue
@@ -2,12 +2,14 @@
   <div class="home">
    <HeroSection />
    <CategorySection />
+   <FeatureSection />
   </div>
 </template>
 
 <script setup lang="ts">
 import HeroSection from '~/components/sections/HeroSection.vue';
 import CategorySection from '~/components/sections/CategorySection.vue';
+import FeatureSection from '~/components/sections/FeatureSection.vue';
 
 definePageMeta({
   layout: 'default',
